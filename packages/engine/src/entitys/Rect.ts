8c66b5bee8653807124a19d78eec4e581115ecae
lizhou <lizhou1021@qq.com>
2024-11-11 23:44:43 +0800

    feat: add stroke and styles

--- packages/engine/src/entitys/Rect.ts
+++ packages/engine/src/entitys/Rect.ts
@@ -1,32 +1,63 @@
 import { Runtime } from "../runtime";
 import type { Vector2D } from "../types";
 import { Entity } from "./Entity";
+import Color from "color";
 
 export class Rect extends Entity {
-	private _width: number;
-	private _height: number;
-	constructor(options?: {
-		width: number;
-		position?: Vector2D;
-		height: number;
-	}) {
-		super(options);
-		this._width = options?.width || 10;
-		this._height = options?.height || 10;
-	}
-	draw(): void {
-		super.draw();
-		const canvas = Runtime.surface.getCanvas();
-		const paint = Runtime.paint;
-		paint.setColor(Runtime.canvasKit.Color(0, 0, 255, 1.0));
-		paint.setStyle(Runtime.canvasKit.PaintStyle.Fill);
+  private _width: number;
+  private _height: number;
+  private _styles = {
+    backgroundColor: "red",
+    strokeColor: "#000",
+    strokeWidth: 10,
+  };
+  constructor(options?: {
+    width: number;
+    position?: Vector2D;
+    height: number;
+  }) {
+    super(options);
+    this._width = options?.width || 10;
+    this._height = options?.height || 10;
+  }
+  draw(): void {
+    super.draw();
+    const canvas = Runtime.surface.getCanvas();
+    const fillPaint = new Runtime.canvasKit.Paint();
+    const stokePaint = new Runtime.canvasKit.Paint();
+    const Skia = Runtime.canvasKit;
 
-		const rect = Runtime.canvasKit.LTRBRect(
-			this._position.x,
-			this._position.y,
-			this._position.x + this._width,
-			this._position.y + this._height,
-		);
-		canvas.drawRect(rect, paint);
-	}
+    const bgColor = Color(this._styles.backgroundColor).object();
+    const strokeColor = Color(this._styles.strokeColor).object();
+
+    fillPaint.setColor(
+      Runtime.canvasKit.Color(bgColor.r, bgColor.g, bgColor.b, bgColor.alpha)
+    );
+    fillPaint.setStyle(Runtime.canvasKit.PaintStyle.Fill);
+
+    const rect = Runtime.canvasKit.XYWHRect(
+      this._position.x,
+      this._position.y,
+      this._position.x + this._width,
+      this._position.y + this._height
+    );
+
+    stokePaint.setStyle(Runtime.canvasKit.PaintStyle.Stroke);
+    stokePaint.setColor(
+      Runtime.canvasKit.Color(
+        strokeColor.r,
+        strokeColor.g,
+        strokeColor.b,
+        strokeColor.alpha
+      )
+    );
+    stokePaint.setStrokeWidth(this._styles.strokeWidth);
+
+    canvas.drawRect(rect, fillPaint);
+
+    canvas.drawRect(rect, stokePaint);
+
+    fillPaint.delete();
+    stokePaint.delete();
+  }
 }
