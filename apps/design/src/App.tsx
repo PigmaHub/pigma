cd2f88426345c4453b9108340225128e976e287a
lizhou <lizhou1021@qq.com>
2024-11-29 23:03:36 +0800

    chore: add twailwind

--- apps/design/src/App.tsx
+++ apps/design/src/App.tsx
@@ -1,13 +1,14 @@
+import "./index.css";
 import "./App.css";
 import { Designer } from "./components/designer";
 
 // src/canvasKitInit.js
 function App() {
-
-
-  return (
-    <Designer />
-  );
+	return (
+		<div class="w-full h-full">
+			<Designer />
+		</div>
+	);
 }
 
 export default App;
