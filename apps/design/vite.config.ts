114f5018a70eee3b85c33d1b5e494831ec416e18
Zhou Li <33168975+ZoeLeee@users.noreply.github.com>
2025-04-12 15:12:28 +0800

    upgrade pkg

--- apps/design/vite.config.ts
+++ apps/design/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [solid()],
-})
+  plugins: [solid(), tailwindcss()],
+});
